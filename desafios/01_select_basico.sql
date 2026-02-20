8868802dcf8107ed1b873403bd44428e9a40c03b
Juliano Silva dos Santos <arandasjuliano@gmail.com>
2026-02-20 11:03:22 -0300

    Add basic SELECT queries for person_person table

---
+++ desafios/01_select_basico.sql
@@ -0,0 +1,11 @@
+-- Aula 02 - SELECT básico
+-- Objetivo: consultar dados da tabela person_person
+
+SELECT *
+FROM person_person;
+
+SELECT FirstName, LastName
+FROM person_person;
+
+SELECT FirstName, LastName, PersonType
+FROM person_person;
