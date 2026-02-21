e4e5eb0c8be98e1dc5fd440387601c6974c77c4d
Juliano Silva dos Santos <arandasjuliano@gmail.com>
2026-02-21 11:21:59 -0300

    Delete desafios/01_select_basico.sql

--- desafios/01_select_basico.sql
+++
@@ -1,5 +0,0 @@
- 'A equipe de marketing precisa fazer uma pesquisa sobre nomes mais comuns de seus clientes e 
- precisa do nome e sobrenome de todos os clientes que estão cadastrados no sistema.'
-
-SELECT firstname, lastname
-FROM person_person
